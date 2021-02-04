dabab53ae235b09b369e725bc1a2d378cca7854c
Erik Rintala <me@erikrintala.com>
2021-02-03 23:02:10 -0800

    First commit for Project 1

---
+++ DataModelingWithPostgres/sql_queries.py
@@ -0,0 +1,54 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id INT NOT NULL, start_time INT, \
+    user_id INT, level VARCHAR, song_id INT, artist_id INT, session_id INT, location VARCHAR, user_agent VARCHAR, PRIMARY KEY(songplay_id));
+""")
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id INT NOT NULL, first_name VARCHAR, last_name VARCHAR, gender VARCHAR, \
+    level VARCHAR, PRIMARY KEY(user_id));
+""")
+
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id INT NOT NULL, title VARCHAR, artist_id INT, year INT, duration int, PRIMARY KEY (song_id));
+""")
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id INT NOT NULL, name VARCHAR, location VARCHAR, latitude INT, longitude INT, PRIMARY KEY(artist_id));
+""")
+
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time INT NOT NULL, hour INT, day INT, week INT, month INT, year INT, weekday VARCHAR, PRIMARY KEY(start_time));
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+""")
+
+user_table_insert = ("""
+""")
+
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+
+time_table_insert = ("""
+""")
+
+# FIND SONGS
+
+song_select = ("""
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
